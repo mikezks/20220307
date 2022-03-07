c0c6ded2ab47e96d931a63c706b65416f8a4c921
mikezks <23002661+mikezks@users.noreply.github.com>
2022-03-07 17:57:31 +0100

    feat: ddd luggage-ui-card

--- libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
+++ libs/luggage/feature-checkin/src/lib/luggage-feature-checkin.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
 import { CheckinComponent } from './checkin.component';
+import { LuggageUiCardModule } from '@flight-workspace/luggage/ui-card';
 
 @NgModule({
-  imports: [CommonModule, LuggageDomainModule],
+  imports: [CommonModule, LuggageDomainModule, LuggageUiCardModule],
   declarations: [CheckinComponent],
   exports: [CheckinComponent],
 })
