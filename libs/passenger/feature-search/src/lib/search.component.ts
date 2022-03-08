ea7b4469e26e49d1eb36034da88f423578f98d2d
mikezks <23002661+mikezks@users.noreply.github.com>
2022-03-08 13:31:33 +0100

    fix: passenger selection

--- libs/passenger/feature-search/src/lib/search.component.ts
+++ libs/passenger/feature-search/src/lib/search.component.ts
@@ -19,6 +19,6 @@ export class SearchComponent {
   }
 
   toggleSelection(p: Passenger) {
-    this.selectedPassenger = p;
+    this.selectedPassenger = p === this.selectedPassenger ? undefined : p;
   }
 }
