83cf4aedd87d26a9117a29cff07ae2e4c0cf1686
mikezks <23002661+mikezks@users.noreply.github.com>
2022-03-09 15:42:56 +0100

    feat: standalone component

--- apps/flight-app/src/app/app.component.ts
+++ apps/flight-app/src/app/app.component.ts
@@ -1,6 +1,11 @@
 import {Component} from '@angular/core';
+import * as FlightBookingModule from './flight-booking';
 
 @Component({
+  /* standalone: true,
+  imports: [
+    FlightBookingModule
+  ], */
   selector: 'flight-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
---
+++ apps/flight-app/src/app/flight-booking/index.ts
@@ -0,0 +1,6 @@
+export * from './flight-search/flight-search.component';
+export * from './flight-card/flight-card.component';
+export * from './passenger-search/passenger-search.component';
+export * from './flight-edit/flight-edit.component';
+export * from './flight-booking.component';
+export * from './flight-typeahead/flight-typeahead.component';
